74f8f9e2817a5979797b49b97a3727bd55fe6d80
Jeffrey Jose <jeffjosejeff@gmail.com>
2020-09-05 23:01:34 -0700

    connect to firestore (publiclist)

---
+++ lib/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+
+    Query query = FirebaseFirestore.instance.collection('publiclist');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: query.snapshots(),
+          builder: (context, stream) {
+            if (stream.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+
+            }
+
+            if (stream.hasError) {
+              return Center(child: Text(stream.error.toString()));
+            }
+
+            QuerySnapshot querySnapshot = stream.data;
+
+            return ListView.builder(
+              itemCount: querySnapshot.size,
+              itemBuilder: (context, index) => Container(height: 20, child: Center(child: Text(querySnapshot.docs[index].data()['name'])),
+            ));
+
+          }
+      )
+    ); 
+  }
+}
